ebd4030e4d97ad5ba96769d22a4d8a79d95e46eb
Milo <JosephMRDominguez@gmail.com>
2025-04-30 01:38:48 -0700

    Configured Google Test

--- include/shared_include/Message.hpp
+++ include/shared_include/Message.hpp
@@ -6,7 +6,6 @@
 class Message{
     private:
         std::string _messageContents;
-
         std::string _to_user_name;
         int _to_user_id;
         std::string _from_user_name;
@@ -19,39 +18,39 @@ class Message{
         //Atomic-safe hashmap of ChatroomID: ChatroomObject
         //Atomic-safe hashmap of UserID : UserName
     public:
-        Message(): _messageContents(""), _to_user_name(""), _to_user_id(-1), _from_user_name(""), _from_user_id(-1), _to_chatroom_id(-1), _from_chatroom_id(-1), _messageType(MessageType::UNDEFINED){};
+        inline Message(): _messageContents(""), _to_user_name(""), _to_user_id(-1), _from_user_name(""), _from_user_id(-1), _to_chatroom_id(-1), _from_chatroom_id(-1), _messageType(MessageType::UNDEFINED){};
 
-        void setMessageContents(std::string messageContents){
-            _messageContents = std::move(messageContents);
-        }
+        inline void setMessageContents(std::string messageContents) {_messageContents = std::move(messageContents);}
 
-        void setToUsername(std::string username){
-            _to_user_name = std::move(username);
-        }
+        inline std::string getMessageContents() const {return _messageContents;}
 
-        void setToUserID(int userID){
-            _to_user_id = userID;
-        }
+        void setToUsername(std::string username) {_to_user_name = std::move(username);}
 
-        void setFromUsername(std::string username){
-            _from_user_name = std::move(username);
-        }
+        inline std::string getToUsername() const {return _to_user_name;}
 
-        void setFromUserID(int userID){
-            _from_user_id = userID;
-        }
+        inline void setToUserID(int userID) {_to_user_id = userID;}
 
-        void setToChatroomID(int chatroomID){
-            _to_chatroom_id = chatroomID;
-        }
+        inline int getToUserID() const {return _to_user_id;}
 
-        void setFromChatroomID(int chatroomID){
-            _from_chatroom_id = chatroomID;
-        }
+        inline void setFromUsername(std::string username) {_from_user_name = std::move(username);}
 
-        void setMessageType(MessageType messageType){
-            _messageType = std::move(messageType);
-        }
+        inline std::string getFromUsername() const {return _from_user_name;}
+
+        inline void setFromUserID(int userID) {_from_user_id = userID;}
+
+        inline int getFromUserID() const {return _from_user_id;}
+
+        inline void setToChatroomID(int chatroomID) {_to_chatroom_id = chatroomID;}
+
+        inline int getToChatroomID() const {return _to_chatroom_id;}
+
+        inline void setFromChatroomID(int chatroomID) {_from_chatroom_id = chatroomID;}
+
+        inline int getFromChatroomID() const {return _from_chatroom_id;}
+
+        inline void setMessageType(MessageType messageType) {_messageType = std::move(messageType);}
+
+        inline MessageType getMessageType() const {return _messageType;}
 };
 
 #endif
--- tests/message_test/MessageTest.cpp
+++ tests/message_test/MessageTest.cpp
@@ -1,4 +1,64 @@
 #include <iostream>
-int main(){
-    std::cout << "Hello World!";
+#include <gtest/gtest.h>
+#include "shared_include/Message.hpp"
+#include "shared_include/MessageBuilder.hpp"
+#include "shared_include/MessageCreator.hpp"
+
+std::string messageContents = "Test Contents";
+std::string toUsername = "Test ToUsername";
+int toUserID = 99;
+std::string fromUsername = "Test FromUsername";
+int fromUserID = 100;
+int toChatroomID = 0;
+int fromChatroomID = 0;
+MessageType messageType = MessageType::LOGIN;
+
+void ExpectDefaultMessageState(const Message&);
+
+void ExpectMessageState(const Message&, const std::string&, const std::string&, const int, const std::string&, const int, const int, const int, const MessageType);
+
+void buildMessage(MessageBuilder&, const std::string&, const std::string&, const int, const std::string&, const int, const int, const int, const MessageType);
+
+TEST(MessageTest, CreateMessageObject){
+    ExpectDefaultMessageState(Message());
+}
+
+TEST(MessageTest, FullMessageBuilderProcess){
+    MessageBuilder messageBuilder = MessageBuilder();
+    ExpectDefaultMessageState(messageBuilder.buildMessage());
+    buildMessage(messageBuilder, messageContents, toUsername, toUserID, fromUsername, fromUserID, toChatroomID, fromChatroomID, messageType);
+    ExpectMessageState(messageBuilder.buildMessage(), messageContents, toUsername, toUserID, fromUsername, fromUserID, toChatroomID, fromChatroomID, messageType);
+}
+
+void ExpectDefaultMessageState(const Message& message){
+    EXPECT_EQ(message.getMessageContents(), "");
+    EXPECT_EQ(message.getToUsername(), "");
+    EXPECT_EQ(message.getToUserID(), -1);
+    EXPECT_EQ(message.getFromUsername(), "");
+    EXPECT_EQ(message.getFromUserID(), -1);
+    EXPECT_EQ(message.getToChatroomID(), -1);
+    EXPECT_EQ(message.getFromChatroomID(), -1);
+    EXPECT_EQ(message.getMessageType(), MessageType::UNDEFINED);
+}
+
+void ExpectMessageState(const Message& message, const std::string& messageContents, const std::string& toUsername, const int toUserID, const std::string& fromUsername, const int fromUserID, const int toChatroomID, const int fromChatroomID, const MessageType messageType){
+    EXPECT_EQ(message.getMessageContents(), messageContents);
+    EXPECT_EQ(message.getToUsername(), toUsername);
+    EXPECT_EQ(message.getToUserID(), toUserID);
+    EXPECT_EQ(message.getFromUsername(), fromUsername);
+    EXPECT_EQ(message.getFromUserID(), fromUserID);
+    EXPECT_EQ(message.getToChatroomID(), toChatroomID);
+    EXPECT_EQ(message.getFromChatroomID(), fromChatroomID);
+    EXPECT_EQ(message.getMessageType(), messageType);
+}
+
+void buildMessage(MessageBuilder& messageBuilder, const std::string& messageContents, const std::string& toUsername, const int toUserID, const std::string& fromUsername, const int fromUserID, const int toChatroomID, const int fromChatroomID, const MessageType messageType) {
+    messageBuilder.setMessageContents(messageContents);
+    messageBuilder.setToUsername(toUsername);
+    messageBuilder.setToUserID(toUserID);
+    messageBuilder.setFromUsername(fromUsername);
+    messageBuilder.setFromUserID(fromUserID);
+    messageBuilder.setToChatroomID(toChatroomID);
+    messageBuilder.setFromChatroomID(fromChatroomID);
+    messageBuilder.setMessageType(messageType);
 }
